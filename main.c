e2952c1f68fc098cfe8117c4a50d593b78e12cad
redboomer <redboomer@gmail.com>
2013-09-11 13:55:39 -0400

    Update main.c
    Got the interrupt accepting input from J1- Pin 15.

--- main.c
+++ main.c
@@ -9,17 +9,6 @@
  * The toggling of the PORT T, Bit 1 output is done via the Compare Result Output
  * Action bits.  
  * 
- * The Output Compare Channel 1 Interrupt is used /******************************************************************************
- * Timer Output Compare Demo
- *
- * Description:
- *
- * This demo configures the timer to a rate of 1 MHz, and the Output Compare
- * Channel 1 to toggle PORT T, Bit 1 at rate of 10 Hz. 
- *
- * The toggling of the PORT T, Bit 1 output is done via the Compare Result Output
- * Action bits.  
- * 
  * The Output Compare Channel 1 Interrupt is used to refresh the Timer Compare
  * value at each interrupt
  * 
@@ -62,6 +51,9 @@
 #define TC1_VAL       ((UINT16)  (((BUS_CLK_FREQ / PRESCALE) / 2) / OC_FREQ_HZ))
 
 
+// Test code.
+UINT16 interruptCounter = 0;
+
 // I prefer the newschool method of declaring functions at the top of the file HR.
 void getMoronsInput(UINT16* lowerBoundaryUs, UINT16* upperBoundaryUs);
 UINT16 getUINT16Input();
@@ -96,20 +88,23 @@ void InitializeTimer(void)
   TSCR2_PR1 = 0;
   TSCR2_PR2 = 0;
     
-  // Enable output compare on Channel 1
-  TIOS_IOS1 = 1;
+  // Change to an input compare. HR 
+  // Enable input capture on Channel 1  
+  TIOS_IOS1 = 0;
   
-  // Set up output compare action to toggle Port T, bit 1
-  TCTL2_OM1 = 0;
-  TCTL2_OL1 = 1;
+  
+  // Set up input capture edge control to capture on a rising edge. 
+  TCTL4_EDG1A = 1;
+  TCTL4_EDG1B = 0;
   
   // Set up timer compare value
-  TC1 = TC1_VAL;
+  //TC1 = TC1_VAL;
   
-  // Clear the Output Compare Interrupt Flag (Channel 1) 
+  // from here down we want this code. HR.
+  // Clear the input capture Interrupt Flag (Channel 1) 
   TFLG1 = TFLG1_C1F_MASK;
   
-  // Enable the output compare interrupt on Channel 1;
+  // Enable the input capture interrupt on Channel 1;
   TIE_C1I = 1;  
   
   //
@@ -145,7 +140,11 @@ void InitializeTimer(void)
 //--------------------------------------------------------------       
 void interrupt 9 OC1_isr( void )
 {
-  TC1     +=  TC1_VAL;      
+  // use this interrupt  
+  
+  ++interruptCounter;
+  
+  //TC1     +=  TC1_VAL;      
   TFLG1   =   TFLG1_C1F_MASK;  
 }
 #pragma pop
@@ -206,7 +205,8 @@ void main(void)
   getMoronsInput(&lowerBoundaryUs, &upperBoundaryUs);
   
   (void)printf("\n lowerBoundaryUs  %u\n",  lowerBoundaryUs);
-  (void)printf("\n upperBoundaryUs  %u\n",  upperBoundaryUs);   
+  (void)printf("\n upperBoundaryUs  %u\n",  upperBoundaryUs);
+  (void)printf("\n interruptCounter  %u\n", interruptCounter);    
   
   // get measurements when user pushes a key.
   
@@ -292,242 +292,4 @@ UINT16 getUINT16Input()
       
    return value;  
 }
- to refresh the Timer Compare
- * value at each interrupt
- * 
- * Author:
- *  Jon Szymaniak (08/14/2009)
- *  Tom Bullinger (09/07/2011)	Added terminal framework
- *
- *****************************************************************************/
-
-
-// system includes
-#include <hidef.h>      /* common defines and macros */
-#include <stdio.h>      /* Standard I/O Library */
-#include <ctype.h>
-
-// project includes
-#include "types.h"
-#include "derivative.h" /* derivative-specific definitions */
-
-// Definitions
-
-// Change this value to change the frequency of the output compare signal.
-// The value is in Hz.
-#define OC_FREQ_HZ    ((UINT16)10)
-
-// Macro definitions for determining the TC1 value for the desired frequency
-// in Hz (OC_FREQ_HZ). The formula is:
-//
-// TC1_VAL = ((Bus Clock Frequency / Prescaler value) / 2) / Desired Freq in Hz
-//
-// Where:
-//        Bus Clock Frequency     = 2 MHz
-//        Prescaler Value         = 2 (Effectively giving us a 1 MHz timer)
-//        2 --> Since we want to toggle the output at half of the period
-//        Desired Frequency in Hz = The value you put in OC_FREQ_HZ
-//
-#define BUS_CLK_FREQ  ((UINT32) 2000000)   
-#define PRESCALE      ((UINT16)  2)         
-#define TC1_VAL       ((UINT16)  (((BUS_CLK_FREQ / PRESCALE) / 2) / OC_FREQ_HZ))
-
-
-// I prefer the newschool method of declaring functions at the top of the file HR.
-void printInstructions(UINT16* lowerBoundaryUs, UINT16* upperBoundaryUs);
-
-
-
-// Initializes SCI0 for 8N1, 9600 baud, polled I/O
-// The value for the baud selection registers is determined
-// using the formula:
-//
-// SCI0 Baud Rate = ( 2 MHz Bus Clock ) / ( 16 * SCI0BD[12:0] )
-//--------------------------------------------------------------
-void InitializeSerialPort(void)
-{
-    // Set baud rate to ~9600 (See above formula)
-    SCI0BD = 13;          
-    
-    // 8N1 is default, so we don't have to touch SCI0CR1.
-    // Enable the transmitter and receiver.
-    SCI0CR2_TE = 1;
-    SCI0CR2_RE = 1;
-}
-
-
-// Initializes I/O and timer settings for the demo.
-//--------------------------------------------------------------       
-void InitializeTimer(void)
-{
-  // Set the timer prescaler to %2, since the bus clock is at 2 MHz,
-  // and we want the timer running at 1 MHz
-  TSCR2_PR0 = 1;
-  TSCR2_PR1 = 0;
-  TSCR2_PR2 = 0;
-    
-  // Enable output compare on Channel 1
-  TIOS_IOS1 = 1;
-  
-  // Set up output compare action to toggle Port T, bit 1
-  TCTL2_OM1 = 0;
-  TCTL2_OL1 = 1;
-  
-  // Set up timer compare value
-  TC1 = TC1_VAL;
-  
-  // Clear the Output Compare Interrupt Flag (Channel 1) 
-  TFLG1 = TFLG1_C1F_MASK;
-  
-  // Enable the output compare interrupt on Channel 1;
-  TIE_C1I = 1;  
-  
-  //
-  // Enable the timer
-  // 
-  TSCR1_TEN = 1;
-   
-  //
-  // Enable interrupts via macro provided by hidef.h
-  //
-  EnableInterrupts;
-}
-
-
-// Output Compare Channel 1 Interrupt Service Routine
-// Refreshes TC1 and clears the interrupt flag.
-//          
-// The first CODE_SEG pragma is needed to ensure that the ISR
-// is placed in non-banked memory. The following CODE_SEG
-// pragma returns to the default scheme. This is neccessary
-// when non-ISR code follows. 
-//
-// The TRAP_PROC tells the compiler to implement an
-// interrupt funcion. Alternitively, one could use
-// the __interrupt keyword instead.
-// 
-// The following line must be added to the Project.prm
-// file in order for this ISR to be placed in the correct
-// location:
-//		VECTOR ADDRESS 0xFFEC OC1_isr 
-#pragma push
-#pragma CODE_SEG __SHORT_SEG NON_BANKED
-//--------------------------------------------------------------       
-void interrupt 9 OC1_isr( void )
-{
-  TC1     +=  TC1_VAL;      
-  TFLG1   =   TFLG1_C1F_MASK;  
-}
-#pragma pop
-
-
-// This function is called by printf in order to
-// output data. Our implementation will use polled
-// serial I/O on SCI0 to output the character.
-//
-// Remember to call InitializeSerialPort() before using printf!
-//
-// Parameters: character to output
-//--------------------------------------------------------------       
-void TERMIO_PutChar(INT8 ch)
-{
-    // Poll for the last transmit to be complete
-    do
-    {
-      // Nothing  
-    } while (SCI0SR1_TC == 0);
-    
-    // write the data to the output shift register
-    SCI0DRL = ch;
-}
-
-
-// Polls for a character on the serial port.
-//
-// Returns: Received character
-//--------------------------------------------------------------       
-UINT8 GetChar(void)
-{ 
-  // Poll for data
-  do
-  {
-    // Nothing
-  } while(SCI0SR1_RDRF == 0);
-   
-  // Fetch and return data from SCI0
-  return SCI0DRL;
-}
-
-
-// Entry point of our application code
-//--------------------------------------------------------------       
-void main(void)
-{
-
-  UINT8 userInput = 0;
-  UINT16 lowerBoundaryUs = 0;
-  UINT16 upperBoundaryUs = 0;
-  
-  InitializeSerialPort();
-  InitializeTimer();
-   
-  // Show initial prompt
-  printInstructions(&lowerBoundaryUs, &upperBoundaryUs);
-  
-  (void)printf("Hello World!\r\n");
-  
-  // get measurements
-  
-  // display results.
-    
-  for(;;)
-  {
-    // Fetch and echo use input
-    userInput = GetChar();
-    (void)printf("%c", userInput);
-  }
-}
-
-
-void printInstructions(UINT16* lowerBoundaryUs, UINT16* upperBoundaryUs) 
-{
-   UINT8 buffer [256];
-   UINT8 userInput2 = 0;
-   INT8 bufferIndex = 0;
-   UINT8 carriageRet = '\r';
-   
-   // Explain the program to the user.
-   (void) printf("This fine piece of crap program will give you a histogram of 1000 rising edge\n");
-   (void) printf("rising edge interarrival times.  It will display the results as a 100 bucket \n");
-   (void) printf("histogram in ascening order, with the lowest arrival time for that bucket displayed.\n\n");
-   
-   // Get the lower range.
-   (void) printf("Please enter the lower range in microseconds. ");
-   
-   // Read the digits into a buffer until you get a carage return.
-   do
-   { 
-      // Fetch and echo use input
-      buffer[bufferIndex] = GetChar();
-      (void)printf("\n userInput2 %u", userInput2);
-      if(isdigit(userInput2)) 
-      {
-        ++bufferIndex; 
-      }
-   }
-   while(userInput2 != carriageRet);
-   
-   // Since the last character is a carriage return replace it with
-   // null so that we can call atoi
-   if (buffer[bufferIndex] == carriageRet )
-   {
-      buffer[bufferIndex] = 0;   
-   }
-  
-  //(*lowerBoundaryUs) = atoi
-  
-  (void) printf("\nPlease enter the upper range in microseconds. ");
-
-}
-  
  
